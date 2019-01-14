3710222b72af4bc0498b1268fdae694acb8ad827
zqf-developer <1454929531@qq.com>
2019-01-14 22:12:53 +0800

    调整结构，新增文件夹

---
+++ study_python_02/formatted_name.py
@@ -0,0 +1,8 @@
+def get_formatted_name(first_name, last_name):
+    """返回整洁的姓名"""
+    full_name = first_name + ' ' + last_name
+    return full_name.title()
+
+
+musician = get_formatted_name('jimi', 'hendrix')
+print(musician)
