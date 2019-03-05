b6cb03ef4f1033cfe6572627ecd1aa0ebdeb2e71
zqf-developer <1454929531@qq.com>
2019-03-05 22:13:30 +0800

    add create a blog web site demo

--- gavin/movie/models.py
+++ gavin/movie/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Movie(models.Model):
+    """映射数据库表"""
+    cover = models.URLField("封面", unique=True)
+    url = models.URLField("链接", unique=True)
+    title = models.CharField("电影标题", max_length=200)
+    info = models.CharField("上演信息", max_length=300)
+    rate = models.FloatField("评分", default=0)
+    commit = models.CharField("短评", max_length=200)
