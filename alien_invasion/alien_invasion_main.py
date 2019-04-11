1f0c537920961bcf169c650eddbd30b3d4940e72
zqf-developer <1454929531@qq.com>
2019-04-11 22:51:20 +0800

    飞船向右移动功能，以及重构代码

--- alien_invasion/alien_invasion_main.py
+++ alien_invasion/alien_invasion_main.py
@@ -1,39 +1,13 @@
-import sys
-
+"""
+alien_invasion这个包名不能与程序名同名，否则import的时候无法生效
+"""
 import pygame
 
 from alien_invasion.settings import Settings
 
+from alien_invasion.ship import Ship
 
-class Ship(object):
-
-    def __init__(self, screen):
-        """初始化飞船并设置其初始位置"""
-        self.screen = screen
-
-        # 加载飞船图像并获取其外接矩形
-        self.image = pygame.image.load('images/ship.bmp')
-        self.rect = self.image.get_rect()
-        self.screen_rect = screen.get_rect()
-
-        # 将每艘新飞船放在屏幕底部中央
-        self.rect.centerx = self.screen_rect.centerx
-        self.rect.bottom = self.screen_rect.bottom
-
-    def blitme(self):
-        """在指定位置绘制飞船"""
-        self.screen.blit(self.image, self.rect)
-
-
-# class Settings():
-#     """存储《外星人入侵》的所有设置类"""
-#
-#     def __init__(self):
-#         """初始化游戏的设置"""
-#         # 屏幕设置
-#         self.screen_width = 1200
-#         self.screen_height = 800
-#         self.bg_color = (230, 230, 230)
+import alien_invasion.game_functions as gf
 
 
 def run_game():
@@ -52,18 +26,8 @@ def run_game():
 
     # 开始游戏的主循环
     while True:
-
-        # 监视键盘和鼠标事件
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-
-        # 每次循环时都重绘屏幕
-        screen.fill(ai_settings.bg_color)
-        ship.blitme()
-
-        # 让最近绘制的屏幕可见
-        pygame.display.flip()
+        gf.check_events(ship)
+        gf.update_screen(ai_settings, screen, ship)
 
 
 run_game()
---
+++ alien_invasion/game_functions.py
@@ -0,0 +1,24 @@
+import sys
+
+import pygame
+
+
+def check_events(ship):
+    """响应按键和鼠标事件"""
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                # 向右移动飞船
+                ship.rect.centerx += 1
+
+
+def update_screen(ai_settings, screen, ship):
+    """更新屏幕上的图像，并切换到新屏幕"""
+    # 每次循环时都重绘屏幕
+    screen.fill(ai_settings.bg_color)
+    ship.blitme()
+
+    # 让最近绘制的屏幕可见
+    pygame.display.flip()
