563c19aab2b8c0a86033b9b64e009c5ad08bd3be
zqf-developer <1454929531@qq.com>
2019-06-11 22:02:52 +0800

    Python-100-Days for two days

---
+++ Day01-15/Day01/turtle.py
@@ -0,0 +1,15 @@
+import this
+import turtle
+
+turtle.pensize(10)  # 箭头大小
+turtle.pencolor('red')  # 箭头颜色
+turtle.speed(1)
+turtle.forward(100)  # 箭头向前进100
+turtle.right(180)  # 箭头转向的角度10度
+turtle.forward(180)
+# turtle.right(10)
+# turtle.forward(100)
+# turtle.right(90)
+# turtle.forward(100)
+turtle.mainloop()
+
---
+++ Day01-15/Day02/circumference_area.py
@@ -0,0 +1,16 @@
+"""
+输入半径计算圆的周长和面积
+
+Version：0.1
+Author: 轻风
+"""
+
+
+import math
+
+
+radius = float(input('请输入圆的半径：'))
+perimeter = 2 * math.pi * radius
+area = math.pi * radius * radius
+print('周长:%.2f' % perimeter)
+print('面积:%.2f' % area)
